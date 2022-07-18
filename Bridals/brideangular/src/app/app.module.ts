8ee51218e6a358f064d98c336ce4e2b76f3fe37f
Charlotte Natasha <natashaonyango97@gmail.com>
2022-07-18 15:24:42 +0200

    added nav details and home components

--- Bridals/brideangular/src/app/app.module.ts
+++ Bridals/brideangular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
